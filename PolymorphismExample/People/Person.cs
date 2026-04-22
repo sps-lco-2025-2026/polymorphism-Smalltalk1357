9ed45d223ee4b30084c2c52b4c0a5bc66c5281fb
Smalltalk1357 <alex.wan@icloud.com>
2026-04-22 20:05:41 +0100

    Created person class

---
+++ PolymorphismExample/People/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace People;
+
+public class Person
+{
+    string Name;
+    string Surname;
+    string Email;
+    DateTime BirthDate;
+    
+    public Person(string name, string surname, string email, DateTime birthDate)
+    {
+        Name = name;
+        Surname = surname;
+        Email = email;
+        BirthDate = birthDate;
+    }
+    
+    
+}
