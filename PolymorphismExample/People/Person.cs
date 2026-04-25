ab91576ebf74f59ac3481a84bb8b70e45c436ad0
Smalltalk1357 <alex.wan@icloud.com>
2026-04-25 20:54:20 +0100

    Added unit tests and updated person class

--- PolymorphismExample/People/Person.cs
+++ PolymorphismExample/People/Person.cs
@@ -1,19 +1,54 @@
-﻿namespace People;
+﻿using System.Runtime.InteropServices.JavaScript;
 
-public class Person
+namespace People;
+
+public class Person(string name, string surname, string email, DateOnly birthday)
 {
-    string Name;
-    string Surname;
-    string Email;
-    DateTime BirthDate;
-    
-    public Person(string name, string surname, string email, DateTime birthDate)
+    public string Name { get; } = name;
+    public string Surname { get; } = surname;
+    public string Email { get; } = email;
+    public DateOnly Birthday { get; } = birthday;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Person(string name, string surname, string email, string birthday) : this(name, surname, email,
+        Validate(birthday))
     {
-        Name = name;
-        Surname = surname;
-        Email = email;
-        BirthDate = birthDate;
     }
     
-    
+    /// <summary>
+    /// Validates if Birthday is a valid date format, otherwise throws an exception.
+    /// </summary>
+    ///
+    private static DateOnly Validate(string birthday)
+    {
+        if (DateOnly.TryParse(birthday, out DateOnly tBirthday))
+        {
+            return tBirthday;
+        }
+        throw new ArgumentException("Invalid date format");
+    }
+
+    /// <summary>
+    /// Calculates the age of the person
+    /// </summary>
+    public int Age()
+    {
+        DateOnly now = DateOnly.FromDateTime(DateTime.Now);
+        int age = now.Year - Birthday.Year;
+        
+        // if the birthday hasn't happened yet this year, subtract 1
+        if (now < Birthday.AddYears(age)) age--;
+        
+        return age;
+    }
+
+    /// <summary>
+    /// Checks if the person is an adult
+    /// </summary>
+    public bool IsAdult()
+    {
+        return Age() >= 18;
+    }
 }
---
+++ PolymorphismExample/Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests;
+using People;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public void TestBirthday()
+    {
+        Person p = new Person("John", "Doe", "", "1990-01-01");
+        Assert.AreEqual(p.Birthday, DateOnly.FromDateTime(new DateTime(1990, 1, 1)) );
+    }
+    
+    [TestMethod]
+    public void TestAge()
+    {
+        Person p = new Person("John", "Doe", "", "1990-01-01");
+        Assert.AreEqual(36, p.Age());
+    }
+    
+    [TestMethod]
+    public void TestBirthdayValidation()
+    {
+        Person p = new Person("John", "Doe", "", "1990-01-01");
+        Assert.AreEqual("", p.Email);
+    }
+}
